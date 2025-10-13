befeae16074bc8954e6ce9e82a0b0a9768b7992d
Usama Ayoub <41838423+sam3690@users.noreply.github.com>
2025-10-13 23:24:11 +0000

    Fix power operation routing

--- public/client.js
+++ public/client.js
@@ -33,7 +33,9 @@ function calculate(operand1, operand2, operation) {
         case '/':
             uri += "?operation=divide";
             break;
-        
+        case '^':
+            uri += "?operation=power";
+            break;
         case 'sqrt':
             uri += "?operation=sqrt";
             break;
