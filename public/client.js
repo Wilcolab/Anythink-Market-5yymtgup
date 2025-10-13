febe3a38586317e3f971290ec8877ff9eec7e95f
Usama Ayoub <41838423+sam3690@users.noreply.github.com>
2025-10-13 23:31:52 +0000

    changes

--- public/client.js
+++ public/client.js
@@ -33,9 +33,6 @@ function calculate(operand1, operand2, operation) {
         case '/':
             uri += "?operation=divide";
             break;
-        case '^':
-            uri += "?operation=power";
-            break;
         case 'sqrt':
             uri += "?operation=sqrt";
             break;
